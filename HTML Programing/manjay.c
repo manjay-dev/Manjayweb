b1628d2678a2e7d934ffe24fa839646dd56dab94
manjay-dev <mrazdev1@gmail.com>
2024-11-13 23:39:56 +0530

    first commit

---
+++ HTML Programing/manjay.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+    {
+int age;
+printf("Enter the age:");
+scanf("%d",&age);
+
+if(age>40)
+{
+printf("the man is adult \n");
+printf("they can vote \n");
+}
+
+else()
+
+
+
+    }
